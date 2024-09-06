eec369cf8cbae6bc21a128fbf24f9473f339c0fe
Ditte Toftegaard Nielsen <ditte@inilab.dk>
2024-09-06 10:13:04 +0200

    change route name

--- src/resources/js/Layouts/AuthenticatedLayout.vue
+++ src/resources/js/Layouts/AuthenticatedLayout.vue
@@ -29,7 +29,7 @@ const showingNavigationDropdown = ref(false);
 
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink :href="route('/')" :active="route().current('/')">
+                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Today's graph
                                 </NavLink>
                                 <NavLink :href="route('dashboard')" :active="route().current('blah')">
