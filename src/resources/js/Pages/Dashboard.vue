d0b1e29bec5c2e4fb6f4936f5c088927a3aca775
Ditte Toftegaard Nielsen <ditte@inilab.dk>
2024-09-06 09:28:10 +0200

    new v-for

--- src/resources/js/Pages/Dashboard.vue
+++ src/resources/js/Pages/Dashboard.vue
@@ -6,6 +6,7 @@ import {OfficeRooms} from "@/Types";
 
 const props = defineProps<{
     officeRoomToday: OfficeRooms[]
+    rooms: OfficeRooms[]
 }>()
 console.log(props.officeRoomToday);
 </script>
@@ -22,7 +23,8 @@ console.log(props.officeRoomToday);
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">Here you can see today's graph for temperatures in the office
-                        <div v-for="row in officeRoomToday" :key="row.id">
+                        <div v-for="row in rooms" :key="row.id">
+                            <p>{{ row.roomName }}</p>
                             <p>{{ row.temp }}</p>
 
                         </div>
