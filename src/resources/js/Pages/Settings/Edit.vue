f5203a10bda12fa9ab5bf62a10080d678dfc6d62
Ditte Toftegaard Nielsen <ditte@inilab.dk>
2024-09-10 12:14:33 +0200

    added another form tag

--- src/resources/js/Pages/Settings/Edit.vue
+++ src/resources/js/Pages/Settings/Edit.vue
@@ -39,43 +39,46 @@ function submit() {
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900"><!--Her kan du se og ændre indstillinger for et gældende lokale -->
                         <div>
-                            <div class="w-full">
+                            <div class="w-full flex">
                                 <div class="w-1/4 border border-red-500"></div>
                                 <h3 class="ml-24">Ændre indstillinger for lokale {{ props.settings.roomName }}</h3>
                             </div>
                             <div>
-                                <div class="mt-3 text-center sm:mt-5">
+                                <form @submit.prevent="submit">
+                                    <div class="mt-3 text-center sm:mt-5">
 
-                                    <div class="flex flex-row px-2 py-2 items-center w-full pb-4 mt-2">
-                                        <InputLabel for="country" class="text-right w-1/4">Interval</InputLabel>
-                                        <TextInput v-model="form.interval" class="w-1/2 ml-5" />
-                                        <InputError :message="form.errors.interval" class="ml-2" />
+                                        <div class="flex flex-row px-2 py-2 items-center w-full pb-4 mt-2">
+                                            <InputLabel for="country" class="text-right w-1/4">Interval</InputLabel>
+                                            <TextInput v-model="form.interval" class="w-1/2 ml-5" />
+                                            <InputError :message="form.errors.interval" class="ml-2" />
+                                        </div>
+                                        <div class="flex flex-row px-2 py-2 items-center w-full pb-4 mt-2">
+                                            <InputLabel for="country" class="text-right w-1/4">Max Temp</InputLabel>
+                                            <TextInput v-model="form.maxTemp" class="w-1/2 ml-5" />
+                                            <InputError :message="form.errors.maxTemp" class="ml-2" />
+                                        </div>
+                                        <div class="flex flex-row px-2 py-2 items-center w-full pb-4 mt-2">
+                                            <InputLabel for="country" class="text-right w-1/4">Min Temp</InputLabel>
+                                            <TextInput v-model="form.minTemp" class="w-1/2 ml-5" />
+                                            <InputError :message="form.errors.minTemp" class="ml-2" />
+                                        </div>
+                                        <div class="flex flex-row px-2 py-2 items-center w-full pb-4 mt-2">
+                                            <InputLabel for="country" class="text-right w-1/4">Start Tid</InputLabel>
+                                            <TextInput v-model="form.startHour" class="w-1/2 ml-5" placeholder="08:00" />
+                                            <InputError :message="form.errors.startHour" class="ml-2" />
+                                        </div>
+                                        <div class="flex flex-row px-2 py-2 items-center w-full pb-4 mt-2">
+                                            <InputLabel for="country" class="text-right w-1/4">Slut Tid</InputLabel>
+                                            <TextInput v-model="form.endHour" class="w-1/2 ml-5" placeholder="16:00"/>
+                                            <InputError :message="form.errors.endHour" class="ml-2" />
+                                        </div>
                                     </div>
-                                    <div class="flex flex-row px-2 py-2 items-center w-full pb-4 mt-2">
-                                        <InputLabel for="country" class="text-right w-1/4">Max Temp</InputLabel>
-                                        <TextInput v-model="form.maxTemp" class="w-1/2 ml-5" />
-                                        <InputError :message="form.errors.maxTemp" class="ml-2" />
+                                    <div class="w-full flex">
+                                        <div class="w-1/4"></div>
+                                        <GreenButton type="submit" @click="submit">Gem indstillinger</GreenButton>
                                     </div>
-                                    <div class="flex flex-row px-2 py-2 items-center w-full pb-4 mt-2">
-                                        <InputLabel for="country" class="text-right w-1/4">Min Temp</InputLabel>
-                                        <TextInput v-model="form.minTemp" class="w-1/2 ml-5" />
-                                        <InputError :message="form.errors.minTemp" class="ml-2" />
-                                    </div>
-                                    <div class="flex flex-row px-2 py-2 items-center w-full pb-4 mt-2">
-                                        <InputLabel for="country" class="text-right w-1/4">Start Tid</InputLabel>
-                                        <TextInput v-model="form.startHour" class="w-1/2 ml-5" placeholder="08:00" />
-                                        <InputError :message="form.errors.startHour" class="ml-2" />
-                                    </div>
-                                    <div class="flex flex-row px-2 py-2 items-center w-full pb-4 mt-2">
-                                        <InputLabel for="country" class="text-right w-1/4">Slut Tid</InputLabel>
-                                        <TextInput v-model="form.endHour" class="w-1/2 ml-5" placeholder="16:00"/>
-                                        <InputError :message="form.errors.endHour" class="ml-2" />
-                                    </div>
-                                </div>
-                                <div class="w-full">
-                                    <div class="w-1/4"></div>
-                                    <GreenButton type="submit" @click="submit">Gem indstillinger</GreenButton>
-                                </div>
+                                </form>
+
                             </div>
                         </div>
                     </div>
