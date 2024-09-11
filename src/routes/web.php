11e276326cf27fdb8df287d42a72e80ffee9017c
Ditte Toftegaard Nielsen <ditte@inilab.dk>
2024-09-11 14:47:18 +0200

    fixing routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -28,7 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-//    Route::get('office/index', [OfficeRoomsController::class, 'index']); // Gets today's data
+    Route::get('/yesterdayGraph', [OfficeRoomsController::class, 'yesterdayData'])->name('yesterdayGraph');
 
     //Setting routes for frontend
     Route::get('setting/index', [SettingsController::class, 'index'])->name('settings.index');
@@ -48,7 +48,7 @@ Route::get('/displayChart', function () {
 Route::get('/settings', [SettingsController::class, 'getSettings']);
 Route::get('/settings/{roomName}', [SettingsController::class, 'getSettingsByRoomName']);
 
-Route::get('/update-sensorUno', [SensorController::class, 'updateSensorUno']);
+
 
 // Define a route for the POST request
 Route::post('/office/store', [OfficeRoomsController::class, 'store']);
